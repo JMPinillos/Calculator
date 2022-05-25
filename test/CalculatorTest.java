559d58d4d4112ade99c97bdc6739a4a8fafdb430
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2022-05-25 17:39:55 +0200

    He creado la clase CalculatorTest, que sera la encargada de realizar las pruebas de nuestro programa. Tambien he introducido el metodo addTest, que se encarga de realizar las pruebas de suma.

---
+++ test/CalculatorTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    //METODO QUE COMPRUEBA SI LA SUMA DE DOS NUMEROS ES CORRECTA
+    @Test
+    public void addTest(){
+        //arrange
+        float result = 7;
+        float number1 = 2;
+        float number2 = 5;
+
+        Calculator calculator = new Calculator();
+
+        //act
+        float calc = calculator.add(number1,number2);
+
+        //assert
+        assertEquals(result, calc);
+    }
+}
