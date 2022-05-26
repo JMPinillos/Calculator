14d8f2944d713d78f29dd5b8618b9e08f33f4bae
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2022-05-26 20:47:14 +0200

    Inserto la multiplicación al trabajo final

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
-    public float add(float num1, float num2){
+    public double add(double num1, double num2){
         return num1 + num2;
     }
     public double multiply(double num1, double num2){
--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -7,14 +7,30 @@ public class CalculatorTest {
     @Test
     public void addTest(){
         //arrange
-        float result = 7;
-        float number1 = 2;
-        float number2 = 5;
+        double result = 7;
+        double number1 = 2;
+        double number2 = 5;
 
         Calculator calculator = new Calculator();
 
         //act
-        float calc = calculator.add(number1,number2);
+        double calc = calculator.add(number1,number2);
+
+        //assert
+        assertEquals(result, calc);
+    }
+
+    @Test
+    public void multiplyTest(){
+        //arrange
+        double result = 19;
+        double number1 = 10;
+        double number2 = 1.9;
+
+        Calculator calculator = new Calculator();
+
+        //act
+        double calc = calculator.multiply(number1,number2);
 
         //assert
         assertEquals(result, calc);
