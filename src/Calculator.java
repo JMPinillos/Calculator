bf7ed1b9c62c1024b9065789f162701d481ad738
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2022-05-26 21:26:25 +0200

    update double

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,8 @@
 public class Calculator {
+
+    public double add(double num1, double num2){
+        return num1 + num2;
+    }
     public double divide(double num1, double num2){
         return num1 / num2;
     }
--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -4,6 +4,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
+    //METODO QUE COMPRUEBA SI LA SUMA DE DOS NUMEROS ES CORRECTA
+    @Test
+    public void addTest(){
+        //arrange
+        double result = 7;
+        double number1 = 2;
+        double number2 = 5;
+
+        Calculator calculator = new Calculator();
+
+        //act
+        double calc = calculator.add(number1,number2);
+
+        //assert
+        assertEquals(result, calc);
+    }
+
     //METODO QUE COMPRUEBA SI LA DIVISION DE DOS NUMEROS ES CORRECTA
     @Test
     public void divideTest(){
