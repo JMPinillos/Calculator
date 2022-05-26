2ac74430712f7612b2080811135e7e4bb5f7fa27
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2022-05-26 21:12:54 +0200

    update

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
-    public double add(double num1, double num2){
-        return num1 + num2;
+    public double divide(double num1, double num2){
+        return num1 / num2;
     }
 }
