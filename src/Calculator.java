bd6e636d318a3ffae0003783966d7e38d6797b2d
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2022-05-26 21:07:14 +0200

    Hacemos merge de subtract y multiply

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,6 +3,10 @@ public class Calculator {
         return num1 + num2;
     }
 
+    public float subtract(float num1, float num2){
+        return num1-num2;
+    }
+
     public double multiply(double num1, double num2){
         return num1 * num2;
     }
