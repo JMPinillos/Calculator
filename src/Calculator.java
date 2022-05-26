8ad60cce6a5ed3e955f6cac7836c55d0fe264982
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2022-05-26 20:51:17 +0200

    Inserto la multiplicación al trabajo final

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,7 @@
 public class Calculator {
-    public double add(double num1, double num2){
+    public double add(double num1, double num2) {
         return num1 + num2;
     }
-    public double multiply(double num1, double num2){
-        return num1 * num2;
-    }
 }
+
 
--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -19,20 +19,4 @@ public class CalculatorTest {
         //assert
         assertEquals(result, calc);
     }
-
-    @Test
-    public void multiplyTest(){
-        //arrange
-        double result = 19;
-        double number1 = 10;
-        double number2 = 1.9;
-
-        Calculator calculator = new Calculator();
-
-        //act
-        double calc = calculator.multiply(number1,number2);
-
-        //assert
-        assertEquals(result, calc);
-    }
 }
