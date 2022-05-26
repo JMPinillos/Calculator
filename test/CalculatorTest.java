a421d3532d9723f44f75b6e9ebfe64040747b887
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2022-05-26 20:43:20 +0200

    Despues de implementar el método todo funciona correctamente

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -3,27 +3,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
-    //METODO QUE COMPRUEBA SI LA SUMA DE DOS NUMEROS ES CORRECTA
-    @Test
-    public void addTest(){
-        //arrange
-        float result = 7;
-        float number1 = 2;
-        float number2 = 5;
-
-        Calculator calculator = new Calculator();
-
-        //act
-        float calc = calculator.add(number1,number2);
-
-        //assert
-        assertEquals(result, calc);
-    }
-
+    //METODO QUE COMPRUEBA SI LA MULTIPLICACION DE DOS NUMEROS ES CORRECTA
     @Test
     public void multiplyTest(){
         //arrange
-        double result = 8.1;
+        double result = 19;
         double number1 = 10;
         double number2 = 1.9;
 
