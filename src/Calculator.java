c11809f175a13ada65aa4607464fd4fba9a00ac4
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2022-05-26 21:12:54 +0200

    update

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,15 +1,5 @@
 public class Calculator {
-    public double add(double num1, double num2) {
+    public double add(double num1, double num2){
         return num1 + num2;
     }
-
-    public float subtract(float num1, float num2){
-        return num1-num2;
-    }
-
-    public double multiply(double num1, double num2){
-        return num1 * num2;
-    }
 }
-
-
