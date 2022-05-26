878d5e1a9e970e7ee40bf4032fd7d463eee6ec1d
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2022-05-26 21:10:55 +0200

    update

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -7,9 +7,9 @@ public class CalculatorTest {
     @Test
     public void addTest(){
         //arrange
-        double result = 7;
-        double number1 = 2;
-        double number2 = 5;
+        double result = -2.5;
+        double number1 = 2.5;
+        double number2 = -5;
 
         Calculator calculator = new Calculator();
 
@@ -20,37 +20,5 @@ public class CalculatorTest {
         assertEquals(result, calc);
     }
 
-    //METODO QUE COMPRUEBA SI LA RESTA DE DOS NUMEROS ES CORRECTA
-    @Test
-    public void subtractTest(){
-        //arrange
-        float result = 5;
-        float number1 = 10;
-        float number2 = 5;
 
-        Calculator calculator = new Calculator();
-
-        //act
-        float calc = calculator.subtract(number1,number2);
-
-        //assert
-        assertEquals(result, calc);
-    }
-
-    //METODO QUE COMPRUEBA SI LA MULTIPLICACION DE DOS NUMEROS ES CORRECTA
-    @Test
-    public void multiplyTest(){
-        //arrange
-        double result = 19;
-        double number1 = 10;
-        double number2 = 1.9;
-
-        Calculator calculator = new Calculator();
-
-        //act
-        double calc = calculator.multiply(number1,number2);
-
-        //assert
-        assertEquals(result, calc);
-    }
 }
