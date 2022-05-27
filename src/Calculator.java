71c5eedb721ae5da64226b8a0deb870610c4aa49
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2022-05-27 21:57:28 +0200

    UPDATE

--- src/Calculator.java
+++ src/Calculator.java
@@ -25,8 +25,17 @@ public class Calculator {
     }
 
     // Método para calcular la raíz cuadrada de un número de precisión doble.
-    
+
     public double sqrt(double num1){
-        return Math.sqrt(num1);
+        int antSqrt = 0;
+        int postSqrt = 0;
+        double calc = 0;
+
+        while(postSqrt*postSqrt <= num1){
+            antSqrt = postSqrt;
+            postSqrt++;
+        }
+        calc = ((antSqrt*antSqrt*antSqrt*antSqrt)+(6*antSqrt*antSqrt*num1)+(num1*num1)) / ((4*antSqrt*antSqrt*antSqrt)+(4*antSqrt*num1));
+        return calc;
     }
 }
