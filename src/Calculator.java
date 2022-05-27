43a53f927fcc5f9b853085aa587ce57319afc9d9
pelayocampa <pelayo@pelayocampa>
2022-05-27 21:18:55 +0200

    Añadido código raíz cuadrada

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,18 +1,32 @@
 public class Calculator {
 
+    // Método para calcular la suma de dos números de precisión double.
+
     public double add(double num1, double num2){
         return num1 + num2;
     }
+
+    // Método para calcular la resta de dos números de precisión double.
 
     public float subtract(float num1, float num2){
         return num1-num2;
     }
 
+    // Método para calcular la multiplcación de dos números de precisión double.
+
     public double multiply(double num1, double num2){
         return num1 * num2;
     }
 
+    // Método para calcular la division de dos números de precisión double.
+
     public double divide(double num1, double num2){
         return num1 / num2;
+    }
+
+    // Método para calcular la raíz cuadrada de un número de precisión doble.
+    
+    public double sqrt(double num1){
+        return Math.sqrt(num1);
     }
 }
