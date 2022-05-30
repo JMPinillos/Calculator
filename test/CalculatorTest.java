01df7be79bdae8551d0369a03f6863e4c2505ec8
Jose Manuel Pinillos Rubio <75635580+JMPinillos@users.noreply.github.com>
2022-05-30 08:28:54 +0200

    Update CalculatorTest.java
    Limpieza de metodos innecesarios en esta rama

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -5,74 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CalculatorTest {
 
-    //METODO QUE COMPRUEBA SI LA SUMA DE DOS NUMEROS ES CORRECTA
-    @Test
-    public void addTest(){
-        //arrange
-        double result = 7;
-        double number1 = 2;
-        double number2 = 5;
-
-        Calculator calculator = new Calculator();
-
-        //act
-        double calc = calculator.add(number1,number2);
-
-        //assert
-        assertEquals(result, calc);
-    }
-    //METODO QUE COMPRUEBA SI LA RESTA DE DOS NUMEROS ES CORRECTA
-    @Test
-    public void subtractTest(){
-        //arrange
-        float result = 5;
-        float number1 = 10;
-        float number2 = 5;
-
-        Calculator calculator = new Calculator();
-
-        //act
-        float calc = calculator.subtract(number1,number2);
-
-        //assert
-        assertEquals(result, calc);
-    }
-
-    //METODO QUE COMPRUEBA SI LA MULTIPLICACION DE DOS NUMEROS ES CORRECTA
-    @Test
-    public void multiplyTest(){
-        //arrange
-        double result = 19;
-        double number1 = 10;
-        double number2 = 1.9;
-
-        Calculator calculator = new Calculator();
-
-        //act
-        double calc = calculator.multiply(number1,number2);
-
-        //assert
-        assertEquals(result, calc);
-    }
-
-    //METODO QUE COMPRUEBA SI LA DIVISION DE DOS NUMEROS ES CORRECTA
-    @Test
-    public void divideTest(){
-        //arrange
-        double result = -2.5;
-        double number1 = 5;
-        double number2 = -2;
-
-        Calculator calculator = new Calculator();
-
-        //act
-        double calc = calculator.divide(number1,number2);
-
-        //assert
-        assertEquals(result, calc);
-    }
-
-    // Método que comprueba si la raíz cuadrada de un número es correcta
+    //METODO QUE COMPRUEBA SI LA RAIZ CUADRADA DE UN NUMERO ES CORRECTA CON UN ERROR DE 10^-3
     @Test
     public void squareRootTest(){
         // arrange
